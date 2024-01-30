bb11b22fc8d85db1be56f362c3fc014b9ab6fb31
ChristianMoraDiaz <christianmoradiaz@hotmail.com>
2024-01-30 17:49:07 -0500

    Fix(DB): fixed database schema and added test user

--- prisma/migrations/20240130215423_first/migration.sql
+++ prisma/migrations/20240130215423_first/migration.sql
@@ -15,8 +15,9 @@ CREATE TABLE "Task" (
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
