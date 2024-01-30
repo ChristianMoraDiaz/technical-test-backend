8e5b904fc90dbaaab71ff659aa5516d0cc97d53f
ChristianMoraDiaz <christianmoradiaz@hotmail.com>
2024-01-30 11:46:49 -0500

    feat(core); Setup Cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
@@ -6,6 +7,8 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
 
 app.get("/ping", (_req, res) => {
   console.log("someone ping here!!");
